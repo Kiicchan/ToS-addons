4605825dc203b2e61ad8ded9c6d2afac034d6257
Marcos Chacon <marcoschacon39@gmail.com>
2022-07-30 18:19:45 -0300

    automatch v.1.0.2

--- automatch/addon_d.ipf/automatch/automatch.lua
+++ automatch/addon_d.ipf/automatch/automatch.lua
@@ -1,13 +1,13 @@
 local addonName = "AUTOMATCH"
 local author = "Kiicchan"
-local version = "1.0.1"
+local version = "1.0.2"
 
 _G["ADDONS"] = _G["ADDONS"] or {}
 _G["ADDONS"][author] = _G["ADDONS"][author] or {}
 _G["ADDONS"][author][addonName] = _G["ADDONS"][author][addonName] or {}
 local g = _G["ADDONS"][author][addonName];
 local acutil = require("acutil");
-local amList = {cm1 = 644, cm2 = 645, cm3 = 646, sing = 647, moring = 608, moringponia = 608, glacia = 619, giltine = 643, res = 636, relic = 636}
+local amList = {cm = 646, sing = 647, moring = 608, moringponia = 608, glacia = 619, res = 636, reshard = 643 , relic = 636, relichard = 643, giltine = 635, medusa = 656, vasilisa = 656}
 
 CHAT_SYSTEM("AutoMatch by Kiicchan");
 
@@ -31,14 +31,14 @@ end
 
 function AUTOMATCH_HELP()
 	local text = "";
-	text = "{s18}/automatch cm1{/}{nl} {s16}CM Area 1: Lvl 361~399{nl} {nl}";
-	text = text .. "{s18}/automatch cm2{/}{nl} {s16}CM Area 2: Lvl 400+{nl} {nl}";
-	text = text .. "{s18}/automatch cm3{/}{nl} {s16}CM Area 3: Lvl 450+{nl} {nl}";
-	text = text .. "{s18}/automatch sing{/}{nl} {s16}Division Singularity{nl} {nl}";
-	text = text .. "{s18}/automatch moring{/}{nl} {s16}Lepdoptera Junction{nl} {nl}";
-	text = text .. "{s18}/automatch glacia{/}{nl} {s16}White Witch's Forest{nl} {nl}";
-	text = text .. "{s18}/automatch giltine{/}{nl} {s16}Demonic Sanctuary{nl} {nl}";
-	text = text .. "{s18}/automatch res sacrae{/}{nl} {s16}Res Sacrae Dungeon";
+	text = "{s18}/am cm{/}{nl} {s16}CM Automatch 440+{nl} {nl}";
+	text = text .. "{s18}/am sing{/}{nl} {s16}Division Singularity{nl} {nl}";
+	text = text .. "{s18}/am moring{/}{nl}{s18}/am moringponia{/}{nl} {s16}Lepdoptera Junction{nl} {nl}";
+	text = text .. "{s18}/am glacia{/}{nl} {s16}White Witch's Forest{nl} {nl}";
+	text = text .. "{s18}/am giltine{/}{nl} {s16}Demonic Sanctuary{nl} {nl}";
+	text = text .. "{s18}/am res{/}{nl}{s18}/am relic{/}{nl} {s16}Res Sacrae Dungeon (Normal) {nl} {nl}";
+	text = text .. "{s18}/am reshard{/}{nl}{s18}/am relichard{/}{nl} {s16}Res Sacrae Dungeon (Hard) {nl} {nl}";
+	text = text .. "{s18}/am vasilisa{/}{nl}{s18}/am medusa{/}{nl} {s16}Saint's Sacellum {nl} {nl}";
 	return ui.MsgBox(text,"","Nope");
 end
 
@@ -61,7 +61,7 @@ function AUTOMATCH_OPEN_DUNGEON(cmd)
 	redButton:SetUserValue('MOVE_INDUN_CLASSID', indunCls.ClassID);
 	if _G[redButtonScp] ~= nil then
 		_G[redButtonScp](frame, redButton)
-		ReserveScript("AUTOMATCH_ENTER()", 3.5)
+		ReserveScript("AUTOMATCH_ENTER()", 0.5)
 	end		
 end
 
